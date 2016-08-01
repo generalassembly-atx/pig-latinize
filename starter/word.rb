4241b76eaa05a4d2f2ecadc3baab9af1a18679fd
camille morell <cemorell>
2016-08-01 18:45:26 -0500

    done by 7 with the help of one Mike Dang

--- starter/word.rb
+++ starter/word.rb
@@ -2,5 +2,20 @@ class Word
   def initialize(original_word)
     @original_word = original_word.downcase
   end
-
+  def first_letter
+    @original_word[0]
+  end
+  def to_pig
+    vowels = ["a", "e", "i" , "o", "u"]
+    if vowels.include? first_letter
+      puts "#{@original_word}way"
+    elsif
+      original_first_letter = first_letter
+      word = @original_word
+      word[0] = ''
+      puts "#{word}#{original_first_letter}ay"
+    end
+  end
 end
+camille = Word.new "helpful"
+camille.to_pig
