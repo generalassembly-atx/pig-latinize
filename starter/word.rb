ce23984461c4f3a4aa8ec3623c0e504f6e68e0bb
Christian Mansour <c.mansour@miami.edu>
2016-08-01 19:07:46 -0500

    piglatinize done

--- starter/word.rb
+++ starter/word.rb
@@ -2,5 +2,15 @@ class Word
   def initialize(original_word)
     @original_word = original_word.downcase
   end
-
+  def piglatinize
+    if (@original_word.start_with? 'a', 'e', 'i', 'o', 'u')
+      "#{@original_word}way"
+    else
+      first_letter = @original_word[0]
+      rest_of_word = @original_word[1..-1]
+      "#{rest_of_word}#{first_letter}ay"
+    end
+  end
 end
+
+NewWord = Word.new "eagle"
