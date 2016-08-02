190eae9d8142c608869f24d2fb5d88c6a4f3e0c0
Susannah Waite <susannahwaite2@gmail.com>
2016-08-01 19:16:34 -0500

    pig-latin homework complete

--- starter/word.rb
+++ starter/word.rb
@@ -2,5 +2,26 @@ class Word
   def initialize(original_word)
     @original_word = original_word.downcase
   end
+  #if original word begins with a, e, i, o, u add way
+  def to_pig_latin
+    if @original_word.start_with? 'a', 'e', 'i', 'o', 'u'
+      "#{@original_word}way"
+  #else take first letter of string move to the end
+  #add ay
+    else
+      original_letter_group = @original_word[0..first_vowel-1]
+      rest_of_word = @original_word[first_vowel..-1]
+      puts "#{rest_of_word}#{original_letter_group}ay"
+    end
+  end
 
+  def first_vowel
+    @original_word.index(/[aeiou]/)
+  end
 end
+
+Word.new("egg").to_pig_latin
+Word.new("susannah").to_pig_latin
+
+
+
