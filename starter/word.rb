8e5cfe6cdf382d25194388a29cddd929281429a5
Kelvan Ince <incekelvan@gmail.com>
2016-11-21 17:31:26 -0600

    Finished homework

--- starter/word.rb
+++ starter/word.rb
@@ -1,6 +1,20 @@
 class Word
+  attr_accessor :original_word
+
   def initialize(original_word)
     @original_word = original_word.downcase
   end
 
+  def piglatinize()
+    if (@original_word.start_with? 'a', 'e', 'i', 'o', 'u')
+      puts "#{@original_word}ay"
+    else
+      puts @original_word
+    end
+  end
+
 end
+
+new_word = Word.new('ok')
+
+puts new_word.piglatinize
