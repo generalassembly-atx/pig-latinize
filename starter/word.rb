29de84c8d07a05553bc2bed397dd41cd6108679c
Clark Sanford <clrksanford@gmail.com>
2016-11-21 16:48:25 -0600

    Did homework

--- starter/word.rb
+++ starter/word.rb
@@ -3,4 +3,43 @@ class Word
     @original_word = original_word.downcase
   end
 
+  def piglatinize
+    first_letter = @original_word.slice(0)
+
+    if (first_letter =~ /[aeiou]/)
+
+      "#{@original_word}way"
+
+    else
+
+      @original_word.slice!(1, @original_word.length) + first_letter + "ay"
+
+    end
+  end
 end
+
+word = Word.new('eight')
+sentence = gets.chomp
+
+def piglatinize(original_word)
+  first_letter = original_word.slice(0)
+  second_letter = original_word.slice(1)
+
+  if (first_letter =~ /[aeiou]/)
+
+    "#{original_word}way"
+
+  elsif (second_letter =~ /[aeiou]/)
+
+    original_word.slice!(1, original_word.length) + first_letter + "ay"
+
+  else
+
+    original_word.slice!(2, original_word.length) + first_letter + second_letter + "ay"
+
+  end
+end
+
+entencesay = sentence.split.map {|word| piglatinize(word)}
+
+puts entencesay.join(' ')
