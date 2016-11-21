75d72149b53c0e687f2f308a6710fa3fa74aff4d
Aaron Baker <aaronbaker9210@gmail.com>
2016-11-21 16:35:42 -0600

    tests fleshed out

--- starter/word.rb
+++ starter/word.rb
@@ -4,22 +4,42 @@ class Word
   end
 
   def piglatinize
-
+    
   end
 
 end
 
 
 test1 = Word.new('egg')
+puts "Expected: eggway"
+puts "Got: #{test1.piglatinize}"
 test2 = Word.new('inbox')
+puts "Expected: inboxway"
+puts "Got: #{test2.piglatinize}"
 test3 = Word.new('eight')
+puts "Expected: eightway"
+puts "Got: #{test3.piglatinize}"
 
 test4 = Word.new('happy')
+puts "Expected: appyhay"
+puts "Got: #{test4.piglatinize}"
 test5 = Word.new('duck')
+puts "Expected: uckday"
+puts "Got: #{test5.piglatinize}"
 test6 = Word.new('glove')
+puts "Expected: oveglay"
+puts "Got: #{test6.piglatinize}"
 
 test7 = Word.new('yellow')
+puts "Expected: ellowyay"
+puts "Got: #{test7.piglatinize}"
 test8 = Word.new('rhythm')
+puts "Expected: ythmrhay"
+puts "Got: #{test8.piglatinize}"
 
 test9 = Word.new('queen')
+puts "Expected: eenquay"
+puts "Got: #{test9.piglatinize}"
 test10 = Word.new('squeal')
+puts "Expected: ealsquay"
+puts "Got: #{test10.piglatinize}"
