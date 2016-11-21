d713b1a83348cf0f8889f028a5672adccc94d30c
Aaron Baker <aaronbaker9210@gmail.com>
2016-11-21 16:28:36 -0600

    setting up tests

--- starter/word.rb
+++ starter/word.rb
@@ -8,3 +8,18 @@ class Word
   end
 
 end
+
+
+test1 = Word.new('egg')
+test2 = Word.new('inbox')
+test3 = Word.new('eight')
+
+test4 = Word.new('happy')
+test5 = Word.new('duck')
+test6 = Word.new('glove')
+
+test7 = Word.new('yellow')
+test8 = Word.new('rhythm')
+
+test9 = Word.new('queen')
+test10 = Word.new('squeal')
