8934dd27bec28509763f67c681da8b92df480709
Connor Garber <czaneg@gmail.com>
2016-11-22 08:35:14 -0600

    hw complete

--- starter/word.rb
+++ starter/word.rb
@@ -1,6 +1,22 @@
 class Word
+  attr_accessor :original_word
+
   def initialize(original_word)
     @original_word = original_word.downcase
   end
 
+  def piglatinize
+    newWord = original_word
+    if newWord[0] =~ /[aeiou]/
+      "#{newWord}way"
+    else
+      first_letter = newWord.slice!(0)
+      "#{first_letter}#{newWord}ay"
+    end
+
+  end
+
 end
+
+pigWord = Word.new('mapple')
+puts pigWord.piglatinize
