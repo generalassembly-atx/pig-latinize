18212ab85928fdcb0d9e915bba462c7c4d44bca1
David Leseten <davidleseten@davids-mbp.attlocal.net>
2016-11-21 20:26:11 -0600

    homework solution

--- starter/word.rb
+++ starter/word.rb
@@ -3,4 +3,18 @@ class Word
     @original_word = original_word.downcase
   end
 
+  def piglatinize
+    new_word = @original_word
+    if (@original_word[0].match(/a|e|i|o|u/).nil?)
+      new_word = new_word + new_word[0] + "way"
+      new_word.slice!(0)
+      return new_word
+    else
+      new_word = new_word+"way"
+    end
+  end
+
 end
+
+word = Word.new('headcase')
+puts word.piglatinize
